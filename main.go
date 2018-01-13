9666dcaffd96311f9d8e132efe13b4ab7d9ce562
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-01-13 19:06:57 +0000

    inits autocompleter and some basic cmds

--- main.go
+++ main.go
@@ -10,12 +10,19 @@ import (
 	"github.com/chzyer/readline"
 )
 
+var completer = readline.NewPrefixCompleter(
+	readline.PcItem("echo"),
+	readline.PcItem("help"),
+	readline.PcItem("ps"),
+)
+
 func main() {
 	kubecontext, err := kubectl("config", "current-context")
 	if err != nil {
 		panic(err)
 	}
 	rl, err := readline.NewEx(&readline.Config{
+		AutoComplete:    completer,
 		Prompt:          fmt.Sprintf("[\033[32m%s\033[0m]$ ", kubecontext),
 		HistoryFile:     "/tmp/readline.tmp",
 		InterruptPrompt: "^C",
@@ -40,11 +47,23 @@ func main() {
 		}
 		line = strings.TrimSpace(line)
 		switch {
+		case line == "help":
+			usage(rl.Stderr())
+		case strings.HasPrefix(line, "echo"):
+			fmt.Println("printing the content of an environment variable")
+		case strings.HasPrefix(line, "ps"):
+			fmt.Println("listing your distributed processes running in the cluster")
 		case line == "exit" || line == "quit":
 			goto exit
+		case line == "":
 		default:
 			fmt.Println("unknown command", strconv.Quote(line))
 		}
 	}
 exit:
+}
+
+func usage(w io.Writer) {
+	_, _ = io.WriteString(w, "Currently the following commands are available:\n")
+	_, _ = io.WriteString(w, completer.Tree("    "))
 }
