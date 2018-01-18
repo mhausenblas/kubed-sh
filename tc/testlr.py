27860b8182eedd2f0c8ea289c0e6328cbf2a8b32
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-01-18 15:58:16 +0000

    adds long-running Python test case

---
+++ tc/testlr.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+A simple HTTP server.
+Usage::
+    ./testlr.py
+Send a GET request::
+    curl http://localhost
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        print("handling GET request")
+        self._set_headers()
+        self.wfile.write("<html><body><p>Hello from Python!</p></body></html>")
+
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', 80)
+    httpd = server_class(server_address, handler_class)
+    print('Starting simple Python server...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+    run()
